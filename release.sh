8b7c5854d0eb7f3e4f19ca2c068e19fddea94a4d
Kristian Toldy <kristian.toldy@gmail.com>
2020-05-21 23:31:30 +0200

    Skuska

--- release.sh
+++ release.sh
@@ -1,2 +1,2 @@
-wget -O ./index.html  --post-data="feed=https://www.sme.sk/rss-title" https://webpub.herokuapp.com/
+wget -O ./index.html  --post-data="feed=https://www.sme.sk/rss-title&sort=Sort+feed+by+date" https://webpub.herokuapp.com/
 curl -T ./index.html  -u webpubjecoolnet:Abcde111 ftp://endora.endora.cz/webpub.jecool.net/web/index.html
