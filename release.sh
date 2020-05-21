227268c969e4ff4032beb3fb1672e7786aad6153
Kristian Toldy <kristian.toldy@gmail.com>
2020-05-21 22:29:55 +0200

    V curl

--- release.sh
+++ release.sh
@@ -1,2 +1,2 @@
 wget -O ./index.html  --post-data="feed=https://www.sme.sk/rss-title" https://webpub.herokuapp.com/
-curl -T ./index.html  -u webpubjecoolnet:Abcde123 ftp://webpub.jecool.net/
+curl -T ./index.html  -u webpubjecoolnet:Abcde123 ftp://webpub.jecool.net/ -V
