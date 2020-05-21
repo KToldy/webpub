0b9cab9ac4fed73fa0fbe8baea5cc1285b9f954c
Kristian Toldy <kristian.toldy@gmail.com>
2020-05-21 23:24:52 +0200

    Common

--- release.sh
+++ release.sh
@@ -1,2 +1,2 @@
 wget -O ./index.html  --post-data="feed=https://www.sme.sk/rss-title" https://webpub.herokuapp.com/
-curl -T ./index.html  -u webpubjecoolnet:Abcde123 ftp://endora.endora.cz/webpub.jecool.net/web/index.html
+curl -T ./index.html  -u webpubjecoolnet:Abcde111 ftp://endora.endora.cz/webpub.jecool.net/web/index.html
